5de2fb2fcca2d1723f6888faa445d715d577144a
sergiohdljr <sergio.tjf1@gmail.com>
2022-07-12 18:48:45 -0300

    adicionando itens na lista

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -1,18 +1,20 @@
-const criarTarefa = (evento) =>{
-    
-    evento.preventDefault();
-    const input = document.querySelector("[data-form-input]");
-    const valor = input.value;
+const criarTarefa = (evento) => {
+  evento.preventDefault();
+  const lista = document.querySelector("[data-list]");
+  const input = document.querySelector("[data-form-input]");
+  const valor = input.value;
+  
+  const conteudo = `<p class = "content">${valor}</p> `;
 
-    const tarefa = document.querySelector("[data-task]")
-
-    const conteudo = `<p class = "content">${valor}</p> `
-
-    tarefa.innerHTML = conteudo
-
-    
-}
+  const tarefa = document.createElement("li");
+  tarefa.innerHTML = conteudo; 
+  tarefa.classList.add('task')
+  
+  lista.appendChild(tarefa)
+  input.value = " ";
+  
+};
 
 const novaTarefa = document.querySelector("[data-form-button]");
 
-novaTarefa.addEventListener('click',criarTarefa)
+novaTarefa.addEventListener("click", criarTarefa);
